eec100fa93b398223547e17bc7e77fb8ea2c87c1
J-Breno <joao.breno85@hotmail.com>
2025-08-23 05:48:35 -0300

    Fazendo o meu Header da aplicação

---
+++ portifolio-pessoal/components/Header.tsx
@@ -0,0 +1,22 @@
+export default function Header() {
+  return (
+    <header className="h-[60px] w-full bg-gradient-to-r from-[#2C2E45] to-[#545D70] flex items-center justify-center rounded-t-lg">
+      <nav>
+        <ul className="flex gap-8 text-white font-semibold">
+          {["HOME", "HABILIDADES", "SOBRE", "PROJETOS", "CONTATO"].map((item) => (
+            <li key={item} className="relative group">
+              <a
+                href={`#${item.toLowerCase()}`}
+                className="transition-colors duration-300 hover:text-[#FFAE00] px-1"
+              >
+                {item}
+                {/* Linha animada */}
+                <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#FFAE00] transition-all duration-500 group-hover:w-full"></span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- portifolio-pessoal/src/app/page.tsx
+++ portifolio-pessoal/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Header from "../../components/Header";
+
 export default function Home() {
   return (
     <>
     <div className="w-full bg-gradient-to-b from-[#323B45] to-[#101623] pt-[60px] pb-[45px]">
       <main className="mb-[60px] mx-[150px] w-[1280px] min-h-[3000px] bg-gray-800 rounded-lg shadow-[-10px_10px_40px_0px_rgba(0,0,0,1)]">
-  
+        <Header />
       </main>
     </div>
     </>
