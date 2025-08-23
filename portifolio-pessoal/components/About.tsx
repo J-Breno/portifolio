80d730d0de1333ed7c866bd0db2cc66f0042ee3d
J-Breno <joao.breno85@hotmail.com>
2025-08-23 18:34:50 -0300

    Finalizando o sobre mim

---
+++ portifolio-pessoal/components/About.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+
+export function About() {
+  return (
+    <section id="sobre" className="w-full">
+      <div className="h-[60px] flex items-center px-4 bg-gradient-to-r from-black/95 to-transparent">
+        <h2 className="text-4xl font-bold text-white">Sobre mim</h2>
+      </div>
+
+      <div className="px-6 py-8 space-y-6">
+        <p className="text-[22px] text-white leading-relaxed font-light">
+          Olá, eu sou o Breno, <strong className="font-semibold text-[#FFAE00]">programador fullstack</strong> apaixonado por tecnologia e pela criação de soluções inteligentes. Gosto de desenvolver projetos completos que impactem positivamente a experiência do usuário.
+        </p>
+
+        <p className="text-[22px] text-white leading-relaxed font-light">
+          Ao longo da minha trajetória, trabalhei em <strong className="font-semibold text-[#FFAE00]">projetos como e-commerces, sistemas de produtividade</strong> e <strong className="font-semibold text-[#FFAE00]">aplicações completas</strong>, consolidando minhas habilidades práticas e aprendendo a lidar com os desafios do desenvolvimento de software.
+        </p>
+
+        <p className="text-[22px] text-white leading-relaxed font-light">
+        Tive experiência como <strong className="font-semibold text-[#FFAE00]">estagiário em análise de dados</strong> e atualmente atuo como <strong className="font-semibold text-[#FFAE00]">analista de sistemas</strong> em uma empresa de redes de computadores. Estou pronto para contribuir com soluções inovadoras e agregar valor desde o primeiro dia em novos projetos de desenvolvimento.
+        </p>
+        <div className="flex gap-3">
+            <a
+            href="https://github.com/J-Breno/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+                 src="img/github.svg"
+                alt="Logo do github"
+                width={50}
+                height={50}
+            /></a>
+             <a
+            href="https://www.linkedin.com/in/jo%C3%A3o-breno/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+                 src="img/linkedin.svg"
+                alt="Logo do Linkedin"
+                width={50}
+                height={50}
+            /></a>
+             <a
+            href="https://www.instagram.com/jbrenojj?igsh=MTdzcDFzczVvYmdveA%3D%3D&utm_source=qr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+                 src="img/instagram.svg"
+                alt="Logo do Instagram"
+                width={50}
+                height={50}
+            />
+            </a>
+        </div>
+      </div>
+    </section>
+  );
+}
--- portifolio-pessoal/src/app/page.tsx
+++ portifolio-pessoal/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { About } from "../../components/About";
 import { Banner } from "../../components/Banner";
 import Header from "../../components/Header";
 
@@ -5,9 +6,10 @@ export default function Home() {
   return (
     <>
     <div className="w-full bg-gradient-to-b from-[#323B45] to-[#101623] pt-[60px] pb-[45px]">
-      <main className="mb-[60px] mx-[150px] w-[1280px] min-h-[3000px] bg-gray-800 rounded-lg shadow-[-10px_10px_40px_0px_rgba(0,0,0,1)]">
+      <main className="mb-[60px] mx-[150px] w-[1280px] min-h-[3000px] bg-[rgba(30,31,36,1)] rounded-lg shadow-[-10px_10px_40px_0px_rgba(0,0,0,1)]">
         <Header />
         <Banner />
+        <About />
       </main>
     </div>
     </>
