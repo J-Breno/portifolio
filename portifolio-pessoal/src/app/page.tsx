55853448484cb7be5f5382df510a7191b870090f
J-Breno <joao.breno85@hotmail.com>
2025-08-24 12:06:29 -0300

    Finalizando Projetos

--- portifolio-pessoal/src/app/page.tsx
+++ portifolio-pessoal/src/app/page.tsx
@@ -2,6 +2,7 @@
 import { About } from "../../components/About";
 import { Banner } from "../../components/Banner";
 import Header from "../../components/Header";
+import Projects from "../../components/projects";
 import Skills from "../../components/Skills";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
         <Banner />
         <About />
         <Skills />
+        <Projects />
       </main>
     </div>
     </>
